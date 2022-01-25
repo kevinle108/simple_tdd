b85e2f7b1f756febd6cf5f74e939c4fc48441269
Kevin Le <kevinle108@gmail.com>
2022-01-25 01:48:48 -0500

    passing tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,37 +1,43 @@
-/** 
+/**
  * @jest-environment jsdom
-*/
+ */
 
+import { render, screen, fireEvent } from '@testing-library/svelte';
 import index from '../routes/index.svelte';
-import {render, screen, fireEvent } from '@testing-library/svelte';
-import axios from 'axios';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 
-it('displays button', () => {
+it('has Load Random User button', () => {
   render(index);
-  const button = screen.getByRole('button', {name: 'Load Random User'});
-  expect(button).toBeInTheDocument();
-});
-
-it('displays title, first and lastname of user',async () => {
-  render(index);
-  const button = screen.getByRole('button', {name: 'Load Random User'});
-  
-  axios.get = jest.fn().mockResolvedValue({
-    data: {
-      results: [
-        {
-          name: {
-            title: 'Mr',
-            first: 'Kevin',
-            last: 'Le'
-          }
-        }
-      ]
-    }
+  const loadButton = screen.queryByRole('button', {
+    name: 'Load Random User'
   });
-  
-  fireEvent.click(button);
+  expect(loadButton).toBeInTheDocument();
+});
+it('displays title, first and lastname of user', async () => {
+  const server = setupServer(
+    rest.get('https://randomuser.me/api', (req, res, ctx) => {
+      return res(
+        ctx.json({
+          results: [
+            {
+              name: {
+                title: 'Mr',
+                first: 'Kevin',
+                last: 'Le'
+              }
+            }
+          ]
+        })
+      );
+    })
+  );
+  server.listen();
+  render(index);
+  const loadButton = screen.queryByRole('button', {
+    name: 'Load Random User'
+  });
+  fireEvent.click(loadButton);
   const userFullName = await screen.findByText('Mr Kevin Le');
-
   expect(userFullName).toBeInTheDocument();
-})
+});
