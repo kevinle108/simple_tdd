502420e45733f7063802dc55b6f7b9a3831b0c6b
Kevin Le <kevinle108@gmail.com>
2022-01-24 23:26:43 -0500

    axios test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,12 +3,35 @@
 */
 
 import index from '../routes/index.svelte';
-import {render, screen} from '@testing-library/svelte';
-import userEvent from '@testing-library/user-event';
-
+import {render, screen, fireEvent } from '@testing-library/svelte';
+import axios from 'axios';
 
 it('displays button', () => {
   render(index);
   const button = screen.getByRole('button', {name: 'Load Random User'});
   expect(button).toBeInTheDocument();
 });
+
+it('displays title, first and lastname of user',async () => {
+  render(index);
+  const button = screen.getByRole('button', {name: 'Load Random User'});
+  
+  axios.get = jest.fn().mockResolvedValue({
+    data: {
+      results: [
+        {
+          name: {
+            title: 'Mr',
+            first: 'Kevin',
+            last: 'Le'
+          }
+        }
+      ]
+    }
+  });
+  
+  fireEvent.click(button);
+  const userFullName = await screen.findByText('Mr Kevin Le');
+
+  expect(userFullName).toBeInTheDocument();
+})
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,26 @@
-<button>Load Random User</button>
+<script lang="ts">
+import axios from "axios";
+
+
+  let user;
+  const loadRandomUser = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api/');
+      console.log(response);
+      user = response.data.results[0];
+    } catch (error) {
+      
+    }
+    // const response = await fetch('https://randomuser.me/api/');
+    // const body = await response.json();
+    // user = body.results[0];
+  }
+</script>
+
+<button on:click="{loadRandomUser}">Load Random User</button>
+{#if user}
+  <h1>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h1>
+{/if}
+<style>
+
+</style>
